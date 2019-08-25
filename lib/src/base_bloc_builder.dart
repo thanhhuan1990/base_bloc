591df9e8ddf9664d8fdc91e9b61f8bee0c33a957
Huan.TEQ <huanhuynh@teqnological.asia>
2019-08-25 15:01:22 +0700

    . Upgrade bloc: 0.15.0, flutter_bloc: 0.21.0

--- lib/src/base_bloc_builder.dart
+++ lib/src/base_bloc_builder.dart
@@ -3,7 +3,7 @@ import 'package:base_bloc/src/base_state.dart';
 import 'package:base_bloc/src/flutter_base_bloc.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-class BaseBlocBuilder<S extends BaseState> extends BlocBuilder<BaseEvent, S> {
-   BaseBlocBuilder(BaseBloc<S> bloc, BlocWidgetBuilder<S> builder)
+class BaseBlocBuilder<B extends BaseBloc<BaseEvent, S>, S extends BaseState> extends BlocBuilder<B, S> {
+   BaseBlocBuilder(BaseBloc<BaseEvent, S> bloc, BlocWidgetBuilder<S> builder)
       : super(bloc: bloc, builder: builder);
 }
