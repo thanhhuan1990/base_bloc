d5dc912c3fd4a7777347d6753fab5b220b9ad268
Jake.Huynh <jake.huynh@shopback.com>
2020-02-19 11:38:54 +0700

    . Add function name() to event to support switch inside bloc

--- lib/src/base_event.dart
+++ lib/src/base_event.dart
@@ -2,18 +2,27 @@
 /// Created by Huan.Huynh on 2019-10-22.
 ///
 /// Copyright © 2019 Huan.Huynh. All rights reserved.
-abstract class BaseEvent {}
+abstract class BaseEvent {
+  String name();
+}
 
 class ErrorEvent extends BaseEvent {
   var error;
 
   ErrorEvent(this.error);
   @override
+
   String toString() => 'Error: ${error.toString()}';
+
+  @override
+  String name() => "ErrorEvent";
 }
 
 class InitialEvent extends BaseEvent {
 
   @override
   String toString() => 'InitialEvent';
+
+  @override
+  String name() => "InitialEvent";
 }
