80cecac7b3e11aabb5b2605581cbc087aee180fa
Jake.Huynh <jake.huynh@shopback.com>
2020-02-20 08:56:24 +0700

    . Revert support switch case because cannot smart cast to Class

--- lib/src/base_event.dart
+++ lib/src/base_event.dart
@@ -2,27 +2,18 @@
 /// Created by Huan.Huynh on 2019-10-22.
 ///
 /// Copyright © 2019 Huan.Huynh. All rights reserved.
-abstract class BaseEvent {
-  String eventName();
-}
+abstract class BaseEvent {}
 
 class ErrorEvent extends BaseEvent {
   var error;
 
   ErrorEvent(this.error);
   @override
-
   String toString() => 'Error: ${error.toString()}';
-
-  @override
-  String eventName() => "ErrorEvent";
 }
 
 class InitialEvent extends BaseEvent {
 
   @override
   String toString() => 'InitialEvent';
-
-  @override
-  String eventName() => "InitialEvent";
 }
